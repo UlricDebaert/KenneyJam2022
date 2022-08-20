a399501f6966e26703884d319ba0b49badeb846f
Alan Kilby <alan.kilby.cerquera@gmail.com>
2022-08-20 16:00:09 +0200

    Mana Bar Logic + Card Template

---
+++ Assets/Scripts/CardUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardUI : MonoBehaviour
+{
+    public TMP_Text cardName;
+    public SpriteRenderer cardImage;
+    public GameObject[] manaList;
+    ObjectStatManager objStatManager;
+
+    private void Start()
+    {
+        objStatManager = GetComponent<ObjectStatManager>();
+
+        cardName.text = objStatManager.cardName;
+
+        cardImage.sprite = objStatManager.cardImage;
+
+        for(int i = 0; i < objStatManager.price; i++)
+        {
+            manaList[i].SetActive(true);
+        }
+    }
+}

32252a975f52350f2899c1443bface07a7ed201a
Alan Kilby <alan.kilby.cerquera@gmail.com>
2022-08-20 16:00:32 +0200

    Hotfix Obj Stat Manager

--- Assets/Scripts/ObjectStatManager.cs
+++ Assets/Scripts/ObjectStatManager.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class ObjectStatManager : MonoBehaviour
 {
+    public string cardName;
+    public Sprite cardImage;
     public float weight;
     public float gravity;
     public float price;
     public PhysicsMaterial2D phyMat;
 
-    private Rigidbody2D objRb;
+    //private Rigidbody2D objRb;
 
     private void Start()
     {
-        objRb = GetComponent<Rigidbody2D>();
-        objRb.mass = weight;
-        objRb.gravityScale = gravity;
-        objRb.sharedMaterial = phyMat;
+        //objRb = GetComponent<Rigidbody2D>();
+        //objRb.mass = weight;
+        //objRb.gravityScale = gravity;
+        //objRb.sharedMaterial = phyMat;
     }
 }
